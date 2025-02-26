a7c6e117f7839362fb932dcd341a755717f19cc8
R404xx <Ravo404.0@gmail.com>
2025-02-26 11:43:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,56 +3,57 @@ const sendBtn = document.querySelector(".bar-wrapper button");
 const messageBox = document.querySelector(".message-box");
 
 let API_URL = "https://api.openai.com/v1/chat/completions";
-let API_KEY =  ${{ secrets.GITHUB_API_KEY }
+let API_KEY = "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"; // Replace with your actual API key
 
 sendBtn.onclick = function () {
-  if(messageBar.value.length > 0){
+  if (messageBar.value.length > 0) {
     const UserTypedMessage = messageBar.value;
     messageBar.value = "";
 
-    let message =
-    `<div class="chat message">
-    <img src="user.png">
-    <span>
-      ${UserTypedMessage}
-    </span>
-  </div>`;
+    let message = `
+      <div class="chat message">
+        <img src="user.png">
+        <span>${User TypedMessage}</span>
+      </div>`;
 
-  let response = 
-  `<div class="chat response">
-  <img src="IMG_3889.png">
-  <span class= "new">...
-  </span>
-</div>`
+    let response = `
+      <div class="chat response">
+        <img src="IMG_3889.png">
+        <span class="new">...</span>
+      </div>`;
 
     messageBox.insertAdjacentHTML("beforeend", message);
+    messageBox.insertAdjacentHTML("beforeend", response);
 
-    setTimeout(() =>{
-      messageBox.insertAdjacentHTML("beforeend", response);
-
-      const requestOptions = {
-        method : "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${{ secrets.TOKEN }`
-        },
-        body: JSON.stringify({
-    "model": "gpt-3.5-turbo",
-    "messages": [
-        {
-            "role": "user", 
-            "content": "Please respond in Kurdish Sorani"
-        }
-    ]
-})
-
-      fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        const ChatBotResponse = document.querySelector(".response .new");
-        ChatBotResponse.innerHTML = data.choices[0].message.content;
-        ChatBotResponse.classList.remove("new");
-      }).catch((error) => {
-        ChatBotResponse.innerHTML = "Opps! An error occured. Please try again"
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${API_KEY}`
+      },
+      body: JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {
+            "role": "user",
+            "content": UserTypedMessage // Send the user's message
+          }
+        ]
       })
+    };
+
+    setTimeout(() => {
+      fetch(API_URL, requestOptions)
+        .then(res => res.json())
+        .then(data => {
+          const ChatBotResponse = document.querySelector(".response .new");
+          ChatBotResponse.innerHTML = data.choices[0].message.content;
+          ChatBotResponse.classList.remove("new");
+        })
+        .catch((error) => {
+          const ChatBotResponse = document.querySelector(".response .new");
+          ChatBotResponse.innerHTML = "Oops! An error occurred. Please try again.";
+        });
     }, 100);
   }
-}
+};
